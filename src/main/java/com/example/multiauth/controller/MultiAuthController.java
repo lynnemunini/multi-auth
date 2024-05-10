94be481d7328b1cfeee730d4953cded1fe849c52
lynnemunini <muninilynne65@gmail.com>
2024-05-10 13:38:17 +0300

    Add the resource controller to the controller package

--- src/main/java/com/example/multiauth/controller/MultiAuthController.java
+++ src/main/java/com/example/multiauth/controller/MultiAuthController.java
@@ -1,4 +1,4 @@
-package com.example.multiauth.service;
+package com.example.multiauth.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
